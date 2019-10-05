aaafc354884986b79073706b43dadbb39fae2d07
GABRIEL BORGES CHIARELO <22309@unifacef.edu.br>
2019-10-05 11:50:14 -0300

    Primeiro commit

---
+++ src/test/java/br/com/unifacef/escola/EscolaApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.unifacef.escola;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EscolaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
